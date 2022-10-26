bf3b6dc8eedfa8e107ebfa1fbc75dab9f5c75691
Philip Lages <psilvaelages@gmail.com>
2022-10-25 23:34:19 -0300

    feat: add requirement 9

---
+++ desafio9.sql
@@ -0,0 +1,5 @@
+SELECT COUNT(hist.song_id) AS quantidade_musicas_no_historico
+FROM SpotifyClone.song_history AS hist
+INNER JOIN SpotifyClone.user AS users
+	ON users.user_id = hist.user_id
+WHERE users.user_name = "barbara Liskov";
